8a41a79b0531f9fbf1c1ecd1305aea54d779bffd
cassio <cassioribeiropereira@gmail.com>
2020-09-11 15:36:39 -0300

    feat(front-end): criar projeto e pagina de login.

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './pages/login';
+import CreateGlobal from './global'
+
+function App() {
+  return (
+    <div className="App">
+     <CreateGlobal />
+     <Login />
+    </div>
+  );
+}
+
+export default App;
